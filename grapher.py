763e46f1ee76c1cfa43ab1b3937dd6f2ebb3eb3f
eceo902 <evan.seeyave@gmail.com>
2020-11-09 21:42:58 -0700

    Editing graph data
    Editing graph data

--- grapher.py
+++ grapher.py
@@ -2,22 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_excel(r'data.xlsx', sheet_name=1, header=2)
-beefData = data.iloc[1986:2090, 1:487]
+palmData = data.iloc[2653:2706, 1:487]
 
-landUseData = beefData.iloc[:, 43]
-greenhouseGasData = beefData.iloc[:, 64]
-acidData = beefData.iloc[:, 97]
-eutrData = beefData.iloc[:, 107]
-waterWithdrawData = beefData.iloc[:, 117]
-waterScData = beefData.iloc[:, 124]
-productionData = beefData.iloc[:, 396]
-combinedData = beefData.iloc[:, [396, 64]]  # , 64]]
+# landUseData = palmData.iloc[:, 43]
+# greenhouseGasData = palmData.iloc[:, 64]
+# acidData = palmData.iloc[:, 97]
+# eutrData = palmData.iloc[:, 107]
+# waterWithdrawData = palmData.iloc[:, 117]
+# waterScData = palmData.iloc[:, 124]
+# productionData = palmData.iloc[:, 396]
+
+
+combinedData = palmData.iloc[:, [43, 107]]  # , 64]]
+
 
 
 def removeDashes(row):
     if row.iloc[0] == '-' or row.iloc[1] == '-':  # or row.iloc[2] == '-':
-        row.iloc[0] = 0
-        row.iloc[1] = 0
+        row.iloc[0] = float("NaN")
+        row.iloc[1] = float("NaN")
         # row.iloc[2] = 0
     return row
 
